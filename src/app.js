f2798268633d7d573558577285546ccb277638e9
r0mmm4k <r0mmm4k@gmail.com>
2019-07-30 18:28:01 +0300

    Changed class components to functional:  - Users  Connect axios and do request

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import * as axios from 'axios';
 
 import Navbar from './components/layout/navbar';
-
-import './app.css';
 import Users from './components/users/users';
 
+import './app.css';
+
 class App extends Component {
+
+	state = {
+		users: [],
+		loading: false
+	};
+
+	async componentDidMount() {
+		this.setState({loading: true});
+		const res = await axios.get('https://api.github.com/users');
+		res.status === 200 && this.setState({users: res.data, loading: false});
+	}
+
 	render() {
+
+		const {users, loading} = this.state;
+
 		return (
 			<div className="app">
 				<Navbar/>
 				<div className="container">
-					<Users/>
+					<Users loading={loading} users={users}/>
 				</div>
 			</div>
 		);
--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,43 +1,23 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import UserItem from './user-item';
 
-class Users extends Component {
+const Users = ({users}) => {
+	const usersElement = users.map(({id, ...user}) => <UserItem key={id} {...user}/>);
+	return (
+		<div className='grid-3'>
+			{usersElement}
+		</div>
+	);
+};
 
-	state = {
-		users: [
-			{
-				id: '1',
-				login: 'mojombo',
-				avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-				html_url: 'https://github.com/mojombo'
-			},
-			{
-				id: '2',
-				login: 'defunkt',
-				avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-				html_url: 'https://github.com/defunkt'
-			},
-			{
-				id: '3',
-				login: 'login',
-				avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-				html_url: 'https://github.com/mojombo'
-			}
-		]
-	};
+Users.defaultProps = {
+	users: []
+};
 
-	render() {
-
-		const usersElement = this.state.users.map(({id, ...user}) => <UserItem key={id} {...user}/>);
-
-		return (
-			<div className='grid-3'>
-				{usersElement}
-			</div>
-		);
-	}
-}
+Users.propTypes = {
+	users: PropTypes.array.isRequired
+};
 
 export default Users;
